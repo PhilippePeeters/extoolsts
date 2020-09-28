a719301d870781641750d051b4d013ba744c374b
Philippe Peeters <philippe.a.peeters@bnpparibasfortis.com>
2020-09-28 14:53:24 +0200

    Delete exception

--- ExA6/src/be/abis/exjava/model/AgeCannotBeNegativeException.java
+++
@@ -1,8 +0,0 @@
-package be.abis.exjava.model;
-
-public  class AgeCannotBeNegativeException  extends Exception {
-	   
-    public AgeCannotBeNegativeException (String errorMessage) {
-       super(errorMessage) ;
-   }
-}
