7e6ce2819578dd62ab5453da7a46ccc4543a20ee
Philippe Peeters <philippe.a.peeters@bnpparibasfortis.com>
2020-09-28 10:09:31 +0200

    Java Tooling - Spring Tool Suite

---
+++ HelloWorld/src/be/abis/helloworld/test/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package be.abis.helloworld.test;
+
+/**
+ * @author Administrateur
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if (args.length > 0) {
+			System.out.println("Hello " + args[0]);
+		}
+	}
+
+}
